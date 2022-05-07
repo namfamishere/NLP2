228bb69456d8b1dda7badf3e05af2a75ca423e38
Nam Fam <102389548+namfamishere@users.noreply.github.com>
2022-05-08 05:28:31 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,11 +3,11 @@ from transformers import BertTokenizer
 DEVICE = "cuda"
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
-VALID_BATCH_SIE = 4
+VALID_BATCH_SIZE = 4
 EPOCHS = 10
 ACCUMULATION = 2
 BERT_PATH = "./input/bert-base-uncased"
-MODEL_PATH = "model.bin"
+MODEL_PATH = "./models/model.bin"
 TRAINING_FILE = "./input/IMDB Dataset.csv"
 TOKENIZER = BertTokenizer.from_pretrained(
     BERT_PATH, 
